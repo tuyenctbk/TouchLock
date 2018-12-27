015409d4567908db0ea2c551917b20ca58416897
tuyenpd4 <tuyenpd4@viettel.com.vn>
2018-12-27 17:18:35 +0000

    181227

--- app/src/main/java/joyfunee/touchlock/activity/DrawPatternLockActivity.kt
+++ app/src/main/java/joyfunee/touchlock/activity/DrawPatternLockActivity.kt
@@ -1,6 +1,11 @@
 package joyfunee.touchlock.activity
 
+import joyfunee.touchlock.R
+
 class DrawPatternLockActivity : BaseActivity() {
 
 
+    override fun initUI() {
+        setContentView(R.layout.activity_draw_pattern_lock)
+    }
 }
--- app/src/main/java/joyfunee/touchlock/activity/MainActivity.kt
+++ app/src/main/java/joyfunee/touchlock/activity/MainActivity.kt
@@ -3,8 +3,7 @@ package joyfunee.touchlock.activity
 import android.os.Bundle
 import joyfunee.touchlock.R
 
-class MainActivity: BaseActivity(){
-
+class MainActivity : BaseActivity() {
 
     override fun initUI() {
         setContentView(R.layout.activity_main)
